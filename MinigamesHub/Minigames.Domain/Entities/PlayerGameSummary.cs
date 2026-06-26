e88b12d8631b227570a57228d92e43ddb498e954
Your Name <your-email@example.com>
2026-06-26 13:01:25 +0400

    Initial files

--- MinigamesHub/Minigames.Domain/Entities/PlayerGameSummary.cs
+++ MinigamesHub/Minigames.Domain/Entities/PlayerGameSummary.cs
@@ -1,1 +1,8 @@
-﻿
+﻿namespace Minigames.Domain.Entities;
+
+public class PlayerGameSummary
+{
+    public int Id { get; set; }
+    public required string PlayerName { get; set; }
+    public PlayerGameSummary GameResult { get; private set; }
+}
